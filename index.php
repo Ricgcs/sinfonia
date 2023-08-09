b6cabebed4bb7494884373fc90aab34623bec780
Ricgcs <rg530755@gmail.com>
2023-08-09 08:43:42 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EAD</title>
+    <link rel="stylesheet" href="estilo.css">
+
+</head>
+<body>
+    <section>
+        <h2></h2>
+        <form method="POST"action="#">
+        <input class="campo" type="text" placeholder="Nome de usuário" required>
+
+            <input class="campo" type="password" placeholder="Senha" required> 
+            <button class="botao">Entrar</button> 
+            <span>Ouu</span>
+            <P class="cadastro">Cadastre-se</p>
+        </form>
+    </section>
+</body>
+</html>
+
+
+
+
+
+
